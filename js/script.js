5fc85c1a8054af9334c0903edaf3498b311beb4e
Slavunyaka <aminog1885@gmail.com>
2022-05-23 13:34:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,139 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// // number = 10;
+// // console.log(number);
+
+// // leftBorderWidth = 10;
+
+// const obj = {
+//     a:50
+// };
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// const obj ={
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b'
+// };
+
+// const b = 'b';
+// arrObj[b] = '1234';
+// console.log(arrObj['b']);  
+
+
+
+// дз№2---------------------------------
+// let storeName = 'ATB';
+// let storeDescription = {
+//     budget: 10000,
+//     employees: ['MAX', 'CLA', 'KRU'],
+//         products: {
+//             num: 'ckay',
+//             prais: 1500
+//         },
+//     open: true
+// }; 
+// END дз№2------------------------------
+
+
+
+
+
+// Блок2/13.
+
+// alert('hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18","18"); 
+// console.log(answer);                  //выводим что получилось в консоль
+// console.log(typeof(answer));         // выводим тип данных.
+// const answer = +prompt("Вам есть 18","18");// Динамическая типизация(преобразуем тип данных)
+// console.log(answer);
+// console.log(typeof(answer));
+
+// const answers = [];
+// // answers[0] = prompt('как ваше имя?', '');
+// // answers[1] = prompt('what is your surname?', '');
+// // answers[2] = prompt('how old you?', '');
+
+// // document.write(answers);
+// console.log(typeof(answers));
+
+// Блок2/14.
+
+// const category = 'toys';
+// console.log(`https://somuel.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`привет, ${user}`);
+
+// Блок2/15.
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");//испю динамич типиз. переводим строку5 в цифру5. Унарный +
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;//постфиксная форма 
+
+
+// // ++incr;
+// // --decr;//префиксная форма
+
+// console.log(incr++);//сначало выводит заданное значение а после добавляет 1 которую мы не видем.
+// console.log(--decr);//сначало минусует заданое значение а после выводит результат.
+
+
+//    && -- логическое И работает когда два или больше значений правдивы.
+//    || -- логическое ИЛИ работает когда один из вариантов правдив.
+
+// const isChecked = true,
+//     //isClose = true;
+//     isClose = false;
+
+// console.log(isChecked && isClose); //если оба эти выражения верны то мы получим true
+
+// const isChecked = true,
+// //const isChecked = false,
+//     //isClose = true;
+//     isClose = false;
+
+// console.log(isChecked || isClose); //если хоть одно выражение true результат будет true.
+
+//! -- оператор отрицания напр.!= не равно.и т.д.
+
+
+
+
+
